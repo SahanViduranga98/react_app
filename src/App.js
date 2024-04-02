131090a9b8fe6542830d79bd49f60b38ed590505
SahanViduranga98 <sahanvidurangassc767@gmail.com>
2024-04-02 12:30:55 +0530

    Set State _ Add Increment & Decrement Button

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/welcome'
 import Message from './components/Message'
+import Adder from './components/adder';
 import React,{ Component } from 'react';
 class App extends Component{
   render(){
   return (
     <div className="App">
-      <Greet name="Sahan" time="afternoon">
+      <Adder></Adder>
+      {/* <Greet name="Sahan" time="afternoon">
       <p>This is children props</p>
-      </Greet>
+      </Greet>z
       <Greet name="Shakila" time="Morning">
         <button>Action1</button>
         </Greet>
       <Welcome name="Symonis" group="our">
       <button>Action2</button>
         </Welcome>
-        <Message Welcome Visitor></Message>
+        <Message Welcome Visitor></Message> */}
     </div>
   );
 }
---
+++ src/components/adder.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Adder extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count:0
+    }
+  }
+  increment(){
+    this.setState({
+      count:this.state.count+1
+    })
+    }
+    decrement(){
+      this.setState({
+        count:this.state.count-1
+      })
+    }
+  
+  render() {
+    return (
+      <div>
+        <div>count :{this.state.count}</div> 
+        <button onClick={()=>this.increment()}>Increment</button>
+        <button onClick={()=>this.decrement()}>Decrement</button>
+      </div>
+    )
+  }
+}
+
+export default Adder
